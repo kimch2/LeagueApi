6a09017f61d4e3fef1176febcf80221e4cd8c6da
Jason Tucker <jtgeek@gmail.com>
2015-04-16 21:35:20 -0400

    Fun Stats Half updated.

--- LeagueApi/Controllers/FunStatsController.cs
+++ LeagueApi/Controllers/FunStatsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using LeagueApi.Helper;
 using LeagueApi.Models;
 using RiotServices;
@@ -26,14 +27,70 @@ namespace LeagueApi.Controllers
 
                 using (var riotDb = new RiotDataContext())
                 {
+                    model.ChampionData = ChampionsService.CallService();
+
                     model.MatchCount = riotDb.Matches.Count();
                     var funStats = riotDb.FunStats().ToList();
+
+                    var wonMatches = riotDb.Teams.Where(x => x.Winner == true);
+                    var lostMatches = riotDb.Teams.Where(x => x.Winner == false);
+                    
+                    var matchData = wonMatches.Join(lostMatches, a => a.MatchId, b => b.MatchId,
+                        (a, b) => new MatchData(a, b))
+                        .ToList();
+
+                    model.WinData.Add(new FunStatsResponse
+                    {
+                        Name = "Baron Kills",
+                        Won = matchData.Count(x => x.WinningTeam.BaronKills > x.LosingTeam.BaronKills),
+                        Lost = matchData.Count(x => x.WinningTeam.BaronKills < x.LosingTeam.BaronKills),
+                        Tied = matchData.Count(x => x.WinningTeam.BaronKills == x.LosingTeam.BaronKills)
+                    });
+
+                    model.WinData.Add(new FunStatsResponse
+                    {
+                        Name = "Dragon Kills",
+                        Won = matchData.Count(x => x.WinningTeam.DragonKills > x.LosingTeam.DragonKills),
+                        Lost = matchData.Count(x => x.WinningTeam.DragonKills < x.LosingTeam.DragonKills),
+                        Tied = matchData.Count(x => x.WinningTeam.DragonKills == x.LosingTeam.DragonKills)
+                    });
+                    
                     model.WinData.Add(funStats.First(x => x.Name == "Minion Kills"));
                     model.WinData.Add(funStats.First(x => x.Name == "Kills"));
                     model.WinData.Add(funStats.First(x => x.Name == "Wards"));
-                }
+
+                    model.GamesWithNoDragons = matchData.Count(x => x.WinningTeam.DragonKills == 0 && x.LosingTeam.DragonKills == 0);
+                    model.GamesWithNoBarons = matchData.Count(x => x.WinningTeam.BaronKills == 0 && x.LosingTeam.BaronKills == 0);
+
+                    var playedChamps = riotDb.Participants.GroupBy(x => x.ChampionId)
+                        .Select(g => new PlayedChampionData {ChampionId = g.Key ?? 0, PlayCount = g.Count()})
+                        .OrderByDescending(x => x.PlayCount);
                     
-                return View(model);
+                    model.MostPlayedChampions = playedChamps.Take(5).ToList();
+                    model.LeastPlayedChampions =
+                        playedChamps.OrderBy(x => x.PlayCount).Take(5).OrderByDescending(x => x.PlayCount).ToList();
+
+                    var participantsAndStats = riotDb.Participants.Join(riotDb.ParticipantStats, p => p.Id, s => s.Id,
+                        (p, s) => new { Participant = p, ParticipantStats = s });
+
+                    var participantsAndStatsWinners = participantsAndStats.Where(x => x.ParticipantStats.Winner == true).GroupBy(x => x.Participant.ChampionId).Select(x => new WinRateChampionData{ ChampionId = x.Key ?? 0, WinCount = x.Count()});
+                    var participantsAndStatsLosers = participantsAndStats.Where(x => x.ParticipantStats.Winner == false).GroupBy(x => x.Participant.ChampionId).Select(x => new WinRateChampionData{ ChampionId = x.Key ?? 0, LossCount = x.Count()});
+
+                    model.WinRateChampionData =
+                        participantsAndStatsWinners.Join(participantsAndStatsLosers,
+                            w => w.ChampionId,
+                            l => l.ChampionId,
+                            (w, l) => new WinRateChampionData
+                            {
+                                ChampionId = w.ChampionId,
+                                WinCount = w.WinCount,
+                                LossCount = l.LossCount
+                            })
+                            .ToList();
+
+
+                    return View(model);
+                }
             }
             catch (Exception e)
             {
@@ -42,5 +99,15 @@ namespace LeagueApi.Controllers
         }
     }
 
-    
+    public class MatchData
+    {
+        public MatchData(Team winningTeam, Team losingTeam)
+        {
+            WinningTeam = winningTeam;
+            LosingTeam = losingTeam;
+        }
+
+        public Team WinningTeam { get; set; }
+        public Team LosingTeam { get; set; }
+    }
 }
--- LeagueApi/Models/FunStatsViewModel.cs
+++ LeagueApi/Models/FunStatsViewModel.cs
@@ -11,5 +11,29 @@ namespace LeagueApi.Models
         }
         public int MatchCount { get; set; }
         public List<FunStatsResponse> WinData { get; set; }
+        public int GamesWithNoBarons { get; set; }
+        public ChampionsResponse ChampionData { get; set; }
+        public List<PlayedChampionData> MostPlayedChampions { get; set; }
+        public List<PlayedChampionData> LeastPlayedChampions { get; set; }
+        public int GamesWithNoDragons { get; set; }
+        public List<WinRateChampionData> WinRateChampionData { get; set; }
+    }
+
+    public class PlayedChampionData
+    {
+        public int ChampionId { get; set; }
+        public int PlayCount { get; set; }
+    }
+
+    public class WinRateChampionData
+    {
+        public int ChampionId { get; set; }
+        public int WinCount { get; set; }
+        public int LossCount { get; set; }
+
+        public double WinRate
+        {
+            get { return (((double) WinCount)/(WinCount + LossCount))*100; }
+        }
     }
 }
